0421e5f79e2fbeaa24f7e9225c9e17551b46a17c
Ron <ronveen@gmail.com>
2021-08-01 13:31:42 +0200

    Initial commit

---
+++ src/main/java/org/ninjaware/errands/errands/ErrandsApplication.java
@@ -0,0 +1,13 @@
+package org.ninjaware.errands.errands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrandsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrandsApplication.class, args);
+	}
+
+}
